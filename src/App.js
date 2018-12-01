1988a197ec47a0df528a912d32b470069c35a9bd
mgmolisani <molisani.m@husky.neu.edu>
2018-12-01 13:59:37 -0500

    adding router lib and clean master on home screen for final project push

--- src/App.js
+++ src/App.js
@@ -48,16 +48,16 @@ export default class App
                     flex: '1 1 auto',
                 })}>
                     {/*/!*<GroceryListDesktop/>*/}
-                    {/*<Calendar selections={this.state.selections}
+                    <Calendar selections={this.state.selections}
                               handleDaySelection={this.handleDaySelection}/>
                     <RecipeList schedule={data.schedule.filter(scheduleItem => {
                         return this.state.selections.some(selection => {
                             return scheduleItem.date.isSame(selection, 'day');
                         });
-                    })}/>*/}
-                    <MyRecipes/>
+                    })}/>
+                    {/*<MyRecipes/>*/}
                 </div>
-                <SchedulingModal/>
+                {/*<SchedulingModal/>*/}
                 {/*<AddToCartModal/>*/}
             </div>
         );
